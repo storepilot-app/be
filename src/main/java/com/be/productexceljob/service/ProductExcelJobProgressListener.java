c7fde23bc104d3a0f1df16625f1e2be4a4e0748b
Unoguna <kimjungho.kr@gmail.com>
2026-07-23 23:08:33 +0900

    [refactor] 불필요한 @FunctionalInterface 어노테이션 제거

--- src/main/java/com/be/productexceljob/service/ProductExcelJobProgressListener.java
+++ src/main/java/com/be/productexceljob/service/ProductExcelJobProgressListener.java
@@ -1,10 +1,6 @@
 package com.be.productexceljob.service;
 
-@FunctionalInterface
 public interface ProductExcelJobProgressListener {
-    ProductExcelJobProgressListener NO_OP = (processedCount, totalCount, stage) -> {
-    };
-
     void onProgress(int processedCount, int totalCount, String stage);
 
     default void onCategoryCompleted(long elapsedMillis) {
